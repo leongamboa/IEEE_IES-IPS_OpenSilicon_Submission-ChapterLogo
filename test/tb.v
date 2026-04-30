b568f945144e86e2e11c00af6f1604b46523833e
leongamboa <vitoleon.gamboa@gmail.com>
2026-04-30 21:50:50 +0800

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_vga_OpenSilicon_SubmissionChapterLogo user_project (
+  tt_um_leongamboa_OpenSilicon_SubmissionChapterLogo user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
